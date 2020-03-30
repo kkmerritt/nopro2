ef2cdd711e0a3a4baf60573a6077eb7bb8c16f42
kev merritt <kkmerritt@gmail.com>
2020-03-30 16:59:47 -0400

    initialize

---
+++ js/nopro.js
@@ -0,0 +1,15 @@
+console.log('connected motherfucker!')
+
+// MONTH LISTING
+var d = new Date();
+var myMonth = d.getMonth();
+var x = document.getElementById("myDate");
+
+if (myMonth === 2){x.innerHTML = "MARCH";}
+if (myMonth === 3){x.innerHTML = "APRIL";}
+if (myMonth === 4){x.innerHTML = "MAY";}
+if (myMonth === 5){x.innerHTML = "JUNE";}
+if (myMonth === 6){x.innerHTML = "JULY";}
+if (myMonth === 7){x.innerHTML = "AUGUST";}
+if (myMonth === 8){x.innerHTML = "SEPTEMBER";}
+// END MONTH LISTING
